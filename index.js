69772ed3aa78ec34f37c7b9a6971a6a45d00e5cb
frametg <frametrigger@gmail.com>
2021-06-09 16:47:17 +0700

    Initial project

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const mobileNumber = '000-000-0000'
 
 const IDCardNumber = '0-0000-00000-00-0'
 const amount = 0
-const payload = generatePayload(mobileNumber, { amount }) //First parameter : mobileNumber || IDCardNumber
+//const payload = generatePayload(mobileNumber, { amount }) //First parameter : mobileNumber || IDCardNumber
+const payload ="000201010212021644851645389267595204701153037645405100.05802TH5907PPETECH6007BANGKOK62760107INVOICE031594546222462902705232021060903044215200000007153537474793298856304C73D"
+
 console.log(payload)
 
 // Convert to SVG QR Code
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+const circularReplacer = () => {
+  
+    // Creating new WeakSet to keep 
+    // track of previously seen objects
+    const seen = new WeakSet();
+      
+    return (key, value) => {
+  
+        // If type of value is an 
+        // object or value is null
+        if (typeof(value) === "object" 
+                   && value !== null) {
+          
+        // If it has been seen before
+        if (seen.has(value)) {
+                 return;
+             }
+               
+             // Add current value to the set
+             seen.add(value);
+       }
+         
+       // return the value
+       return value;
+   };
+};
+
+app.post('/scb/payment/confirm', (req, res) => {
+    //console.log(JSON.stringify(
+    // req, circularReplacer()))
+    //res.send(res)
+    console.log(req.body.body)
+  })
+
+app.listen(3000, () => {
+  console.log('Start server at port 3000.')
+})
